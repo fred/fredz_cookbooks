af09ef717c05125b79a50be2927d7878ece42cb0
Frederico Araujo <fred.the.master@gmail.com>
2012-01-21 11:12:52 +0700

    updated emails

--- networking_basic/metadata.rb
+++ networking_basic/metadata.rb
@@ -1,5 +1,5 @@
-maintainer       "fredz, Tim Smith"
-maintainer_email "fred@frederico-araujo.com, tim.smith@webtrends.com"
+maintainer       "Frederico Araujo, Tim Smith"
+maintainer_email "fred.the.master@gmail.com, tim.smith@webtrends.com"
 license          "Apache 2.0"
 description      "Install Basic Networking Tools and Utilities on Debian, Centos RedHat and Ubuntu"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
