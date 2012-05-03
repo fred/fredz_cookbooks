6bf868bf2689bb8db047d5d1245e7d42972e6bf8
Eric G. Wolfe <wolfe21@marshall.edu>
2012-05-03 16:10:35 -0400

    NOTE: networking_basic on community.opscode.com, still refers to README.rdoc, causing problems for folks trying to upload to chef-server/opscode platform Fixed: invalid platform attribute "rhel".  Added redhat, centos, scientific, amazon

--- networking_basic/recipes/default.rb
+++ networking_basic/recipes/default.rb
@@ -4,19 +4,6 @@
 #
 #
 
-case node[:platform]
-  when "debian", "ubuntu"
-    node[:debian][:install_list].each do |pkg|
-      package pkg do
-        action :install
-        ignore_failure true
-    end
-  end
-  when "rhel", "centos"
-    node[:redhat][:install_list].each do |pkg|
-      package pkg do
-        action :install
-        ignore_failure true
-    end
-  end
+node['networking']['packages'].each do |netpkg|
+  package netpkg
 end
