ca5353b01b1851f569bb7f77cf0b30c3d588e648
Frederico Araujo <fred.the.master@gmail.com>
2013-03-13 22:27:29 +0700

    Deleted some old cookbooks

--- xcache/attributes/default.rb
+++
@@ -1,6 +0,0 @@
-default[:xcache][:mem_size] = "64M"
-default[:xcache][:cpu_count] = "2"
-default[:xcache][:slots] = "16K"
-default[:xcache][:var_size] = "2M"
-default[:xcache][:var_count] = "2"
-default[:xcache][:var_slots] = "16K"
