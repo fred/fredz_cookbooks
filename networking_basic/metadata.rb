39ad0cc5fbbb981fc67b33b5b48371ee1da7a166
Frederico Araujo <fred.the.master@gmail.com>
2013-08-08 17:15:31 +0700

    remove tcptraceroute from centos

--- networking_basic/metadata.rb
+++ networking_basic/metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "fred.the.master@gmail.com, tim.smith@webtrends.com, atomic-pen
 license          "Apache 2.0"
 description      "Install Basic Networking Tools and Utilities on Debian, Centos RedHat and Ubuntu"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.textile'))
-version          "0.0.5"
+version          "0.0.6"
 
 %w{ ubuntu debian centos redhat }.each do |os|
   supports os
